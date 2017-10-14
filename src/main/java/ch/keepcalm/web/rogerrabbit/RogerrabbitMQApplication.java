15c81e11335712e98de3974509afdb1a7095640b
Marcel Widmer <marzelwidmer@gmail.com>
2017-10-14 15:58:38 +0200

    upgrade spring-boot version 1.5.7 add maven plugin to install and download, start RabbitMQ

--- src/main/java/ch/keepcalm/web/rogerrabbit/RogerrabbitMQApplication.java
+++ src/main/java/ch/keepcalm/web/rogerrabbit/RogerrabbitMQApplication.java
@@ -1,5 +1,7 @@
 package ch.keepcalm.web.rogerrabbit;
 
+import io.arivera.oss.embedded.rabbitmq.EmbeddedRabbitMq;
+import io.arivera.oss.embedded.rabbitmq.EmbeddedRabbitMqConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RogerrabbitMQApplication {
 
 	public static void main(String[] args) {
+		EmbeddedRabbitMqConfig config = new EmbeddedRabbitMqConfig.Builder().build();
+		EmbeddedRabbitMq rabbitMq = new EmbeddedRabbitMq(config);
+		rabbitMq.start();
 		SpringApplication.run(RogerrabbitMQApplication.class, args);
 	}
 }
