72831524d840e353e964daee40679b1dfb5d3ef9
Marcel Widmer <marzelwidmer@gmail.com>
2017-10-23 18:42:57 +0200

    some comment

--- src/test/java/ch/keepcalm/web/rogerrabbit/RogerrabbitApplicationTests.java
+++ src/test/java/ch/keepcalm/web/rogerrabbit/RogerrabbitApplicationTests.java
@@ -10,7 +10,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class RogerrabbitApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws InterruptedException {
+//		Thread.sleep(10*1000);
+//		EmbeddedRabbitMqConfig config = new EmbeddedRabbitMqConfig.Builder().build();
+//		EmbeddedRabbitMq rabbitMq = new EmbeddedRabbitMq(config);
+//		rabbitMq.start();
+//		Thread.sleep(30*1000);
+//		rabbitMq.stop();
+
 	}
 
 }
